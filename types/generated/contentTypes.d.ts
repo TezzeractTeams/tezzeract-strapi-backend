f68e0532e21ada6ad8885b19970528539825b71c
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-10 16:57:15 +0530

    remove content

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -476,7 +476,6 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
     >;
     category: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
-    Content: Schema.Attribute.Blocks;
     cover: Schema.Attribute.Media<'images' | 'files'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
