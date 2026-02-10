f8319f364a580580da0545890eed3429147d9ae9
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-10 18:26:39 +0530

    add

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -476,6 +476,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
     >;
     category: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
+    Content: Schema.Attribute.RichText;
     cover: Schema.Attribute.Media<'images' | 'files'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
