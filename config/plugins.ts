c1ded0747ff8e4dc89f90c58559b87b1f27ac0df
Wehan Himsara <wehanhimsara@gmail.com>
2026-02-13 13:23:52 +0530

    add preview 2

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,23 @@
 module.exports = ({ env }) => ({
     upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-          // Optional: put all images in a folder
-          folder: 'strapi',
-        },
-        actionOptions: {
-          upload: {
-            // This ensures Strapi requests a Cloudinary thumbnail for preview
-            use_filename: true,
-            unique_filename: false,
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+            folder: 'strapi',
           },
-          delete: {},
+          actionOptions: {
+            upload: {
+              use_filename: true,
+              unique_filename: false,
+            },
+            delete: {},
+          },
+          defaultTransformations: [
+            { width: 200, height: 200, crop: 'limit' },
+          ],
         },
       },
-    },
   });
